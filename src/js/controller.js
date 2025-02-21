3afde16cce506f3c7d812e59bc715b01443e6e40
jmmarketing <jeff@jeffreymclean.com>
2025-02-20 18:56:50 -0800

    Very miminal MVC built out. Really could have completed this in a single file monorepo, but practice. Will update and submit a README tomorrow.

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,6 +3,9 @@ import newsletterView from "./views/newsletter.js";
 import "core-js";
 import "regenerator-runtime/runtime";
 
-function init() {}
+function init() {
+  newsletterView.initiateSubmitListener();
+  newsletterView.initiateDismissListener();
+}
 
 init();
--- src/js/views/newsletter.js
+++ src/js/views/newsletter.js
@@ -1,24 +1,32 @@
+import successIcon from "../../../assets/images/icon-success.svg";
+
 class newsletterView {
   _dismissButton = document.querySelector("#dismiss-button");
   _newsletterForm = document.querySelector(".newsletter__form");
   _signUpContainer = document.querySelector(".newsletter");
   _successContainer = document.querySelector(".success");
   _input = document.querySelector("#newsletter-input");
+  _email = "";
 
-  constructor() {
+  initiateSubmitListener() {
     this._newsletterForm.addEventListener("submit", (e) => {
       console.log("FIRES");
       e.preventDefault();
 
       const emailInput = this._input.value;
-      //   this._toggleDisplay();
       this._verifyEmailFormat(emailInput);
-    });
 
-    this._dismissButton.addEventListener(
-      "click",
-      this._toggleDisplay.bind(this)
-    );
+      if (!this._email) return;
+
+      this._renderSuccessMessage();
+    });
+  }
+
+  initiateDismissListener() {
+    this._successContainer.addEventListener("click", (e) => {
+      e.target.id == "dismiss-button" && this._toggleDisplay();
+      this._email = "";
+    });
   }
 
   _toggleDisplay() {
@@ -39,7 +47,29 @@ class newsletterView {
     const isValid = regex.test(string);
 
     if (!isValid) this._showError();
-    else this._hideError();
+    else {
+      this._hideError();
+      this._email = string;
+    }
+  }
+
+  _renderSuccessMessage() {
+    const html = `
+    <img src="${successIcon}" alt="Success Icon" />
+        <h1>Thanks for subscribing!</h1>
+        <p>
+          A confirmation email has been sent to
+          <span>${this._email}</span>. Please open it and click the button
+          inside to confirm your subscription.
+        </p>
+        <button id="dismiss-button">Dismiss message</button>
+    `;
+
+    this._input.value = "";
+    this._successContainer.innerHTML = "";
+    this._successContainer.insertAdjacentHTML("afterbegin", html);
+
+    this._toggleDisplay();
   }
 }
 
